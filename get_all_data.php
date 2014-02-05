d2f50a2936885c903272775049b452664703eecc
daviddemello <daviddemello@gmail.com>
2014-02-05 15:06:18 -0500

    really strips the empty CDAT out of empty elements when getting all.

--- get_all_data.php
+++ get_all_data.php
@@ -43,4 +43,4 @@ file_put_contents('xml/ilr_people.xml', '</Data>', FILE_APPEND);
 // Run the XSLT transform on the main xml file, which will fold in the fields from lpad and legacy_ilr_directory_HTML
 $raw_xml = file_get_contents('xml/ilr_people.xml');
 $transformed_xml = 'xml/ilr_profiles_feed.xml';
-file_put_contents($transformed_xml, xslt_transform($raw_xml, get_ilr_profiles_transform_xsl(), 'xml'));
+file_put_contents($transformed_xml, stripEmptyCDATA(xslt_transform($raw_xml, get_ilr_profiles_transform_xsl(), 'xml')));
